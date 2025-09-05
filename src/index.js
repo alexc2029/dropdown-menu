208b5d593d56771440ece2c1953fa2700c9223d8
alexc2029 <114008402+alexc2029@users.noreply.github.com>
2025-09-05 12:24:39 +0300

    Complete first version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles.css";
+
+function addDropdownListeners() {
+	let dropdownButtons = document.getElementsByClassName("dropdown-button");
+	for (let button of dropdownButtons) {
+		button.addEventListener("click", function () {
+			this.classList.toggle("active");
+		});
+	}
+}
+
+addDropdownListeners();
